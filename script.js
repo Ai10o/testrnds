635e1fa4b72aea2ade0439bee679a36548860f04
Ai10o <91592355+Ai10o@users.noreply.github.com>
2024-02-13 18:49:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('calculate').addEventListener('click', calculateNOD);
+    document.getElementById('close-error').addEventListener('click', hideError);
+});
+
 function calculateNOD() {
-    var number1 = parseInt(document.getElementById('number1').value);
-    var number2 = parseInt(document.getElementById('number2').value);
+    var number1 = parseInt(document.getElementById('number1').value, 10);
+    var number2 = parseInt(document.getElementById('number2').value, 10);
 
-
-    if (!number1 || !number2 || parseInt(number1) < 0 || parseInt(number2) < 0) {
-        showError("Значение не может быть пустым или меньше нуля");
+    if (isNaN(number1) || isNaN(number2)) {
+        showError("Оба поля должны содержать числа");
         return;
     }
-    
-    if (number1 === 0 || number2 === 0) {
-        showError("Числа не могут быть равны нулю");
+
+    if (number1 <= 0 || number2 <= 0) {
+        showError("Числа должны быть больше нуля");
         return;
     }
-    
-    var nod = findNOD(number1, number2); 
-    document.getElementById('result').innerText = 'Наибольший общий делитель: ' + nod;
+
+    var NOD = findNOD(number1, number2);
+    document.getElementById('NOD-result').innerText = NOD;
 }
+
 
 function findNOD(a, b) {
     if (b === 0) return a; 
     return findNOD(b, a % b); 
 }
-
 function showError(errorMessage) {
     var errorPopup = document.getElementById('error-popup');
     var errorMessageElement = document.getElementById('error-message');
@@ -33,3 +37,4 @@ function hideError() {
     var errorPopup = document.getElementById('error-popup');
     errorPopup.style.display = 'none';
 }
+v
