bc411527ca98b9096761e4ba03fe03c23e7b851f
Ai10o <91592355+Ai10o@users.noreply.github.com>
2024-02-13 17:17:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('button');
-    button.addEventListener('click', calculateGCD);
-});
-
 function calculateGCD() {
-    const num1 = parseInt(document.getElementById('number1').value, 10);
-    const num2 = parseInt(document.getElementById('number2').value, 10);
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
 
-    if (isNaN(num1) || isNaN(num2)) {
-        alert("Пожалуйста, введите только числа.");
+    if (!number1 || !number2 || parseInt(number1) < 0 || parseInt(number2) < 0) {
+        showError("Значение не может быть пустым или меньше нуля");
         return;
     }
-
-    let a = num1;
-    let b = num2;
-    while (b !== 0) {
-        let t = b;
-        b = a % b;
-        a = t;
+    
+    number1 = parseInt(number1, 10);
+    number2 = parseInt(number2, 10);
+    
+    if (number1 === 0 || number2 === 0) {
+        showError("Числа не могут быть равны нулю");
+        return;
     }
+    
+    while (number2 !== 0) {
+        var temp = number2;
+        number2 = number1 % number2;
+        number1 = temp;
+    }
+    
+    document.getElementById('result').innerText = 'Наибольший общий делитель: ' + number1;
+}
 
-    document.getElementById('result').innerText = `Наибольший общий делитель: ${a}`;
+function showError(errorMessage) {
+    var errorPopup = document.getElementById('error-popup');
+    var errorMessageElement = document.getElementById('error-message');
+    errorMessageElement.textContent = errorMessage;
+    errorPopup.style.display = 'block';
+}
+
+function hideError() {
+    var errorPopup = document.getElementById('error-popup');
+    errorPopup.style.display = 'none';
 }
